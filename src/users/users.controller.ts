680db393c2f748707ab9691ba5b94962d7a4b6cc
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-16 19:56:22 +0800

    feat: create create-user dto for validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+  @Post('signup')
+  createUser(@Body() body: CreateUserDto) {
+    // return this.userService.create(body);
+    console.log(body);
+  }
+}
