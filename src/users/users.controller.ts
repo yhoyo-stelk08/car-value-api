01afe75b50654ff450611a3b2a574b154eef4e0c
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-16 21:14:50 +0800

    feat: create findUser method in UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from 'src/dtos/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
   constructor(private userService: UsersService) {}
+
   @Post('signup')
   createUser(@Body() body: CreateUserDto) {
     return this.userService.create(body.email, body.password);
+  }
+
+  @Get(':id')
+  findUser(@Param('id') id: number) {
+    return this.userService.findOne(id);
   }
 }
