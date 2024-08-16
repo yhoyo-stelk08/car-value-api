679151af50c369e664e7bdf7d855c15296faca2d
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-16 10:47:31 +0800

    feat: create Report entity and wire it up to nest apps

---
+++ src/reports/reports.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  mileage: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  approved: boolean;
+}
