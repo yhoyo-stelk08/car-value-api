a9939e5f5d7c62985cb0264271dcf3471d1e08f3
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-16 21:33:55 +0800

    feat: create the rest CRUD methods. - create function to get all users - create function to get user by id - create function to get user by email - create function to update a user - create function to delete a user
     Changes to be committed:
    	modified:   src/users/users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from 'src/dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -11,8 +20,28 @@ export class UsersController {
     return this.userService.create(body.email, body.password);
   }
 
+  @Get('listUsers')
+  findAllUsers() {
+    return this.userService.findAll();
+  }
+
   @Get(':id')
-  findUser(@Param('id') id: number) {
-    return this.userService.findOne(id);
+  findUserById(@Param('id') id: number) {
+    return this.userService.findOne({ id });
+  }
+
+  @Get()
+  findUserByEmail(@Query('email') email: string) {
+    return this.userService.findOne({ email });
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: number, @Body() body: Partial<CreateUserDto>) {
+    return this.userService.update(id, body);
+  }
+
+  @Delete(':id')
+  removeUser(@Param('id') id: number) {
+    return this.userService.remove(id);
   }
 }
