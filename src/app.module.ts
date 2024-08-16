afe3c64fce1154ad2ff26e201d3f0c8d9b9ae791
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-16 10:41:51 +0800

    feat: create User entity and wire it up to nest apps

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/users.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'yhoyo',
+      password: 'Stelkers08&',
+      database: 'car_value_api',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
