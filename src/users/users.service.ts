cc51c097a58df3788244c7366fd7b28a41d6d636
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-16 21:04:29 +0800

    feat: add find, findOne, update n remove method in UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,8 +7,33 @@ import { User } from './users.entity';
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-  create(email: string, password: string) {
+  async create(email: string, password: string): Promise<User> {
     const user = this.repo.create({ email, password });
-    return this.repo.save(user);
+    return await this.repo.save(user);
+  }
+
+  async findOne(id: number): Promise<User | undefined> {
+    return await this.findOne(id);
+  }
+
+  async find(): Promise<User[]> {
+    return await this.find();
+  }
+
+  async update(id: number, attrs: Partial<User>): Promise<User> {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    Object.assign(user, attrs);
+    return await this.repo.save(user);
+  }
+
+  async remove(id: number): Promise<User> {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return await this.repo.remove(user);
   }
 }
