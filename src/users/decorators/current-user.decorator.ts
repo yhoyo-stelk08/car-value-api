aeac4c442117981f0e1c1e7a84f37a14f0968d59
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-18 13:10:16 +0800

    feat: get currentUser value from request which is set by CurrentUserInterceptor

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -3,6 +3,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 // export const CurrentUser = (...args: string[]) => SetMetadata('current-user', args);
 export const CurrentUser = createParamDecorator(
   (data: never, context: ExecutionContext) => {
-    return 'test';
+    const request = context.switchToHttp().getRequest();
+    return request.currentUser;
   },
 );
