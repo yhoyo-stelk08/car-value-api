48dd1c101298ceed12ee5565c1967e939418a3c5
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-18 18:19:03 +0800

    feat: write test for UsersController and AuthService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from './users.entity';
+import { UsersService } from './users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    // Create fake user object
+    const fakeUser: Partial<User> = {
+      id: 1,
+      email: 'test@example.com',
+      password: 'password',
+    };
+
+    // create fake user service
+    const fakeUserService: Partial<UsersService> = {
+      findOneOrNull: () => Promise.resolve(null),
+      create: (email: string, password: string) =>
+        Promise.resolve(fakeUser as User),
+    };
+
+    // Create and compile a test module with AuthService and fake user service
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: fakeUserService },
+      ],
     }).compile();
 
+    // Get the instance of AuthService
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
+  it('can create an instance of auth service', () => {
     expect(service).toBeDefined();
   });
 });
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,15 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: UsersService;
+  let authService: AuthService;
 
   beforeEach(async () => {
+    // create users service mock
+    const usersServiceMock = {
+      findOneOrNull: jest.fn(),
+      create: jest.fn(),
+      update: jest.fn(),
+      remove: jest.fn(),
+      findAll: jest.fn(),
+      findOne: jest.fn(),
+    };
+
+    // create auth service mock
+    const authServiceMock = {
+      signup: jest.fn(),
+      signin: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        { provide: UsersService, useValue: usersServiceMock },
+        { provide: AuthService, useValue: authServiceMock },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
