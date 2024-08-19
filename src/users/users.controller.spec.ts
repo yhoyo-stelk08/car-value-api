2a5cc7f0a7fd655d4c385c614ffed27dd1dd6e35
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-19 18:39:48 +0800

    feat: write test for user controller. - write test to check function register a new user and set session userId. - write test to sign in a user and set session userId.
     Changes to be committed:
    	modified:   users.controller.spec.ts

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,30 +1,32 @@
+import { CreateUserDto } from '@/dtos/create-user.dto';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersController } from './users.controller';
+import { User } from './users.entity';
 import { UsersService } from './users.service';
+
+// create users service mock
+const usersServiceMock = {
+  findOneOrNull: jest.fn(),
+  create: jest.fn(),
+  update: jest.fn(),
+  remove: jest.fn(),
+  findAll: jest.fn(),
+  findOne: jest.fn(),
+};
+
+// create auth service mock
+const authServiceMock = {
+  signup: jest.fn(),
+  signin: jest.fn(),
+};
 
 describe('UsersController', () => {
   let controller: UsersController;
-  let usersService: UsersService;
-  let authService: AuthService;
+  let usersService: jest.Mocked<UsersService>;
+  let authService: jest.Mocked<AuthService>;
 
   beforeEach(async () => {
-    // create users service mock
-    const usersServiceMock = {
-      findOneOrNull: jest.fn(),
-      create: jest.fn(),
-      update: jest.fn(),
-      remove: jest.fn(),
-      findAll: jest.fn(),
-      findOne: jest.fn(),
-    };
-
-    // create auth service mock
-    const authServiceMock = {
-      signup: jest.fn(),
-      signin: jest.fn(),
-    };
-
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [
@@ -34,11 +36,76 @@ describe('UsersController', () => {
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
-    usersService = module.get<UsersService>(UsersService);
-    authService = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(
+      UsersService,
+    ) as jest.Mocked<UsersService>;
+    authService = module.get<AuthService>(
+      AuthService,
+    ) as jest.Mocked<AuthService>;
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('can create a new user and set session userId', async () => {
+    const session = { userId: null };
+
+    // Mock the user data
+    const createUserDto: CreateUserDto = {
+      email: 'test@example.com',
+      password: 'password',
+    };
+    const createdUser: Partial<User> = { id: 1, ...createUserDto };
+
+    // Mock the service methods
+    authService.signup.mockResolvedValue(createdUser as User);
+
+    // call the controller method
+    const result = await controller.createUser(createUserDto, session);
+
+    // assert the service method was called
+    expect(authService.signup).toHaveBeenCalledWith(
+      createUserDto.email,
+      createUserDto.password,
+    );
+    expect(result).toEqual(createdUser);
+    expect(session.userId).toEqual(createdUser.id);
+  });
+
+  it('should sign in a user and set session userId', async () => {
+    const session = { userId: null };
+
+    // Mock the user data
+    const createUserDto: CreateUserDto = {
+      email: 'test@example.com',
+      password: 'password',
+    };
+
+    const createdUser: Partial<User> = { id: 1, ...createUserDto };
+
+    // Mock the service methods
+    authService.signin.mockResolvedValue(createdUser as User);
+
+    // call the controller method
+    const result = await controller.signin(createUserDto, session);
+
+    // assert the service method was called
+    expect(authService.signin).toHaveBeenCalledWith(
+      createUserDto.email,
+      createUserDto.password,
+    );
+    expect(result).toEqual(createdUser);
+    expect(session.userId).toEqual(createdUser.id);
+  });
+
+  it('should get the current user', async () => {});
+
+  it('should sign out the user', async () => {});
+
+  it('should return an array of users when AuthGuard allows access', async () => {});
+  it('should find user by email', async () => {});
+  it('should find user by id', async () => {});
+  it('should update user data', async () => {});
+  it('should remove user', async () => {});
 });
