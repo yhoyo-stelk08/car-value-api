cdcd24ab4492f9b11ad39d6d011a4d1a4f91d319
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-20 14:54:17 +0800

    feat: write test for user controller - write test for get all users - write test for get user by id - write test for get user by email - write test for create user - write test for update user - write test for delete user
     Changes to be committed:
    	modified:   users.controller.spec.ts

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,6 +1,10 @@
 import { CreateUserDto } from '@/dtos/create-user.dto';
 import { AuthGuard } from '@/guards/auth.guard';
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersController } from './users.controller';
@@ -9,23 +13,29 @@ import { UsersService } from './users.service';
 
 // Create mock AuthGuard
 @Injectable()
-class MockAuthGuard {
+class MockAuthGuard extends AuthGuard {
   canActivate(context: ExecutionContext) {
-    return true; // Always allow access in the test
+    const request = context.switchToHttp().getRequest();
+    // inject mockSession
+    request.session = this.mockSession;
+    return request.session.userId;
+  }
+
+  constructor(private mockSession: any) {
+    super();
   }
 }
 
-// create users service mock
+// Create a mock for UsersService
 const usersServiceMock = {
-  findOneOrNull: jest.fn(),
-  create: jest.fn(),
-  update: jest.fn(),
-  remove: jest.fn(),
   findAll: jest.fn(),
   findOne: jest.fn(),
+  findOneOrNull: jest.fn(),
+  update: jest.fn(),
+  remove: jest.fn(),
 };
 
-// create auth service mock
+// Create a mock for AuthService
 const authServiceMock = {
   signup: jest.fn(),
   signin: jest.fn(),
@@ -33,28 +43,35 @@ const authServiceMock = {
 
 describe('UsersController', () => {
   let controller: UsersController;
-  let usersService: jest.Mocked<UsersService>;
-  let authService: jest.Mocked<AuthService>;
+  let usersService: UsersService;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [
-        { provide: UsersService, useValue: usersServiceMock },
-        { provide: AuthService, useValue: authServiceMock },
+        {
+          provide: UsersService,
+          useValue: usersServiceMock,
+        },
+        {
+          provide: AuthService,
+          useValue: authServiceMock,
+        },
+        {
+          provide: AuthGuard,
+          useFactory: () => new MockAuthGuard({ userId: 1 }),
+        },
       ],
-    })
-      .overrideGuard(AuthGuard)
-      .useClass(MockAuthGuard)
-      .compile();
+    }).compile();
 
     controller = module.get<UsersController>(UsersController);
-    usersService = module.get<UsersService>(
-      UsersService,
-    ) as jest.Mocked<UsersService>;
-    authService = module.get<AuthService>(
-      AuthService,
-    ) as jest.Mocked<AuthService>;
+    usersService = module.get<UsersService>(UsersService);
+    authService = module.get<AuthService>(AuthService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
@@ -62,76 +79,204 @@ describe('UsersController', () => {
   });
 
   it('can create a new user and set session userId', async () => {
-    const session = { userId: null };
+    // mock the session
+    const mockSession = { userId: null };
 
-    // Mock the user data
+    // mock the user data
     const createUserDto: CreateUserDto = {
       email: 'test@example.com',
       password: 'password',
     };
-    const createdUser: Partial<User> = { id: 1, ...createUserDto };
+    const createdUser = { id: 1, ...createUserDto } as User;
 
-    // Mock the service methods
-    authService.signup.mockResolvedValue(createdUser as User);
+    // mock the signup method
+    authServiceMock.signup.mockResolvedValue(createdUser);
 
-    // call the controller method
-    const result = await controller.createUser(createUserDto, session);
+    // create a new user
+    const user = await controller.createUser(createUserDto, mockSession);
 
-    // assert the service method was called
-    expect(authService.signup).toHaveBeenCalledWith(
+    // assertions
+    expect(user).toEqual(createdUser);
+    expect(mockSession.userId).toEqual(createdUser.id);
+    expect(authServiceMock.signup).toHaveBeenCalledWith(
       createUserDto.email,
       createUserDto.password,
     );
-    expect(result).toEqual(createdUser);
-    expect(session.userId).toEqual(createdUser.id);
   });
 
   it('should sign in a user and set session userId', async () => {
     const session = { userId: null };
 
-    // Mock the user data
+    // mocking create user data
     const createUserDto: CreateUserDto = {
       email: 'test@example.com',
       password: 'password',
     };
+    const createdUser = { id: 1, ...createUserDto } as User;
 
-    const createdUser: Partial<User> = { id: 1, ...createUserDto };
+    // mocking signin method
+    authServiceMock.signin.mockResolvedValue(createdUser);
 
-    // Mock the service methods
-    authService.signin.mockResolvedValue(createdUser as User);
+    // sign in a user
+    const user = await controller.signin(createUserDto, session);
 
-    // call the controller method
-    const result = await controller.signin(createUserDto, session);
-
-    // assert the service method was called
-    expect(authService.signin).toHaveBeenCalledWith(
+    // assertions
+    expect(user).toEqual(createdUser);
+    expect(session.userId).toEqual(createdUser.id);
+    expect(authServiceMock.signin).toHaveBeenCalledWith(
       createUserDto.email,
       createUserDto.password,
     );
-    expect(result).toEqual(createdUser);
-    expect(session.userId).toEqual(createdUser.id);
   });
 
   it('should get the current user', async () => {
-    // Mock the current user by applying the mock decorator manually
     const user = 'test@example.com';
-
-    // Call the method directly with the mock user
     const result = await controller.currentUser(user);
-
-    // Assertions
     expect(result).toBe(user);
   });
 
-  it('should sign out the user', async () => {});
+  it('should sign out the user', async () => {
+    const mockSession = { userId: 1 };
+
+    // sign out the user
+    await controller.signOut(mockSession);
+
+    // assertions
+    expect(mockSession.userId).toBeNull();
+  });
 
   it('should return an array of users when AuthGuard allows access', async () => {});
 
-  it('should find user by email', async () => {});
+  it('should find user by email and throw exception if not found', async () => {
+    // mock the user data
+    const user = { id: 1, email: 'test2@example.com' } as User;
 
-  it('should find user by id', async () => {});
+    // mock the usersService findOne method
+    usersServiceMock.findOne.mockResolvedValueOnce(user);
 
-  it('should update user data', async () => {});
+    // Call the method with an existing user
+    const foundUser = await controller.findUserByEmail('test2@example.com');
 
-  it('should remove user', async () => {});
+    // Assertions
+    expect(foundUser).toEqual(user);
+
+    // mock the user email not found
+    usersServiceMock.findOne.mockRejectedValueOnce(
+      new NotFoundException('User not found'),
+    );
+
+    // assert that the method throws an exception
+    await expect(
+      controller.findUserByEmail('test@example.com'),
+    ).rejects.toThrow(NotFoundException);
+
+    // Verify the calls to the mock
+    expect(usersServiceMock.findOne).toHaveBeenCalledWith({
+      email: 'test2@example.com',
+    });
+    expect(usersServiceMock.findOne).toHaveBeenCalledWith({
+      email: 'test@example.com',
+    });
+  });
+
+  it('should find user by id and throw an error if not found', async () => {
+    // Mock the user data
+    const mockUser = {
+      id: 1,
+      email: 'test@example.com',
+      password: 'password',
+    } as User;
+
+    // Mock the usersService findOne method
+    usersServiceMock.findOne.mockResolvedValueOnce(mockUser);
+
+    // Call the method with an existing user
+    const foundUser = await controller.findUserById(1);
+
+    // Assertions
+    expect(foundUser).toEqual(mockUser);
+
+    // Mock the user not found
+    usersServiceMock.findOne.mockRejectedValueOnce(
+      new NotFoundException('User not found'),
+    );
+
+    expect(controller.findUserById(2)).rejects.toThrow(NotFoundException);
+
+    // Verify the calls to the mock
+    expect(usersServiceMock.findOne).toHaveBeenCalledWith({ id: 1 });
+    expect(usersServiceMock.findOne).toHaveBeenCalledWith({ id: 2 });
+  });
+
+  it('should update user data', async () => {
+    const id = 1;
+    const updateUserDto: Partial<CreateUserDto> = {
+      email: 'test2@example.com',
+    };
+
+    // mock the user data
+    const mockUser = {
+      id: 1,
+      email: 'test@example.com',
+      password: 'password',
+    } as User;
+
+    // mock the usersService update method
+    usersServiceMock.update.mockResolvedValueOnce(mockUser);
+
+    // Call the method with an existing user
+    const updatedUser = await controller.updateUser(id, updateUserDto);
+
+    // Assertions
+    expect(updatedUser).toEqual(mockUser);
+
+    // Verify the calls to the mock
+    expect(usersServiceMock.update).toHaveBeenCalledWith(id, updateUserDto);
+
+    // Mock the update method in UsersService to throw a NotFoundException
+    usersServiceMock.update.mockRejectedValueOnce(
+      new NotFoundException('User not found'),
+    );
+
+    // Assert that the controller method throws the correct exception
+    await expect(controller.updateUser(id, updateUserDto)).rejects.toThrow(
+      NotFoundException,
+    );
+
+    // Verify the update method was called correctly
+    expect(usersServiceMock.update).toHaveBeenCalledWith(id, updateUserDto);
+  });
+
+  it('should remove user', async () => {
+    const id = 1;
+
+    // mock the user data
+    const mockUser = {
+      id: 1,
+      email: 'test@example.com',
+    } as User;
+
+    // mock the usersService remove method
+    usersServiceMock.remove.mockResolvedValueOnce(mockUser);
+
+    // remove the user
+    const removedUser = await controller.removeUser(id);
+
+    // assertions
+    expect(removedUser).toEqual(mockUser);
+
+    // Verify the calls to the mock
+    expect(usersServiceMock.remove).toHaveBeenCalledWith(id);
+
+    // mock the user not found
+    usersServiceMock.remove.mockRejectedValueOnce(
+      new NotFoundException('User not found'),
+    );
+
+    // assert that the method throws an exception
+    await expect(controller.removeUser(2)).rejects.toThrow(NotFoundException);
+
+    // Verify the calls to the mock
+    expect(usersServiceMock.remove).toHaveBeenCalledWith(2);
+  });
 });
