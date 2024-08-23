0a58b7a7b4ec54596560d176473e885ad8d096bc
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-23 23:09:14 +0800

    feat: write test for ReportsService

--- src/reports/reports.service.spec.ts
+++ src/reports/reports.service.spec.ts
@@ -1,15 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './reports.entity';
 import { ReportsService } from './reports.service';
 
+// Mock report repository
+const mockReportRepository = () => ({
+  create: jest.fn(),
+  save: jest.fn(),
+  find: jest.fn(),
+  findOneOrFail: jest.fn(),
+  remove: jest.fn(),
+});
 describe('ReportsService', () => {
   let service: ReportsService;
+  let mockRepository: jest.Mocked<Repository<Report>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReportsService],
+      providers: [
+        ReportsService,
+        {
+          provide: getRepositoryToken(Report),
+          useValue: mockReportRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<ReportsService>(ReportsService);
+    mockRepository = module.get<Repository<Report>>(
+      getRepositoryToken(Report),
+    ) as jest.Mocked<Repository<Report>>;
   });
 
   it('should be defined', () => {
