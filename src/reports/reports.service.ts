fa9a2127cea7c314adbddbc17a22ae65960ccf97
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-23 22:36:22 +0800

    feat: add create function to ReportsService

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Report } from './reports.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(createReportDto: CreateReportDto) {
+    const report = this.repo.create(createReportDto);
+    return this.repo.save(report);
+  }
+}
