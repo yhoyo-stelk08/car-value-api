d8a6e71491a2abdf7aefd375ea6b8dd92c1084db
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-23 13:28:21 +0800

    feat: create new dto file called CreateReportDto

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,35 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(2052)
+  year: number;
+
+  @IsNumber()
+  mileage: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsLongitude()
+  lng: number;
+}
