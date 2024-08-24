fa43bfeae9710e75804d4791354416309afbd008
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-24 17:57:55 +0800

    feat: modified the report dto to format response to be sent to the users.

--- src/reports/dtos/report.dto.ts
+++ src/reports/dtos/report.dto.ts
@@ -1,1 +1,31 @@
-export class ReportDto {}
+import { Expose, Transform } from 'class-transformer';
+
+export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  model: string;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  mileage: number;
+
+  @Expose()
+  lng: number;
+
+  @Expose()
+  lat: number;
+
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number;
+}
